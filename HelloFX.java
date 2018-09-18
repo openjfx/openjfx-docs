80e4bb4820b089301a02091a471e84ba77d4b318
Joeri Sykora <joeri.sykora@gluonhq.com>
2018-09-18 14:33:07 +0200

    remove early access parts

--- HelloFX.java
+++ HelloFX.java
@@ -8,9 +8,10 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String version = System.getProperty("java.version");
-        Label l = new Label ("Hello, JavaFX 11, running on "+version);
-        Scene scene = new Scene (l, 640, 480);
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(l, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
