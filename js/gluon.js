d336e5ec73062d36f6bca10eceee982e9c5f3b81
José Pereda <pereda@eii.uva.es>
2018-11-12 18:14:49 +0100

    #20 Added tutorial for IDEs (#21)
    * #20 Added Tutorial for IntelliJ
    
    * issue-20: Added Eclipse tutorial
    
    * issue-20: Changes based on feedback
    
    * issue-20: Update Eclipse tutorial for Gradle
    
    * issue-20: Use constants from js
    
    * issue-20: Added NetBeans tutorial
    
    * issue-20: Add JDK link
    
    * #20: Added JDK/JFX MAJOR and VERSION
    
    * #20: Remove dark background from code tags within alerts
    
    * #20: Use h5 instead of strong
    
    * #20: Add danger style to warning alerts
    
    * #20: Updated IntelliJ doc, updated links, added subitems
    - added constant URL of samples repo
    
    * #20: Tweaks for sidebar: sticky, css, scroll
    
    * #20: Updated NetBeans docs with links to samples, including modular samples
    
    * #20: Updated docs for Eclipse, including modular samples

--- js/gluon.js
+++ js/gluon.js
@@ -1,4 +1,13 @@
 $(function() {
+    // constants
+    var JDK_MAJOR = "11";
+    var JFX_MAJOR = "11";
+    
+    var JDK_VERSION = "11.0.1";
+    var JFX_VERSION = "11.0.1";
+    
+    var SAMPLES = "https://github.com/openjfx/samples/blob/master";
+    
     // Hide all non-active div
     $('.hidden').hide();
     
@@ -34,6 +43,20 @@ $(function() {
         loadContent(this);
     });
     
+    // open group, close rest of groups
+    $(".list-group-item .ref-group").click(function(e) {
+        e.preventDefault();
+        var $target = $(this).closest('li').find("ul.list-group-sub").slideToggle();
+        $('html,body').scrollTop(0);
+        $('ul.list-group-sub').not($target).slideUp();
+    });
+    // close opened groups
+    $(".list-group-item").click(function(e) {
+        if (! $(this).children().length > 0) {
+            $('ul.list-group-sub').slideUp();
+        }
+    });
+    
     // Add href to report a problem buttons
     /*$('a[data-section]').each(function() {
         var emailWithSubject = "https://gluonhq.com/about-us/contact-us/?comment=" + 
@@ -53,6 +76,34 @@ $(function() {
         var os = href.split("-")[0]; // #win or #nix
         $('a[class="nav-link"][data-toggle="tab"][href^="' + os + '"]').each(function() {
             $(this).tab('show');
+        });
+    });
+    
+    // Scroll to anchor from list-group-sub links
+    $(document).on('click', ".scrollto", function(event) {
+        event.preventDefault(); 
+        var anchor = $(this).attr('data-scroll');
+//        $('html,body').animate({ scrollTop: $(anchor).offset().top }, 500); 
+        $('html,body').scrollTop($(anchor).offset().top);
+    });
+    
+    // Replace all constants
+    // sticky sidenav
+    $(document).ready(function() {
+        $('.JDK_MAJOR').each(function() { $(this).text(JDK_MAJOR) });
+        $('.JFX_MAJOR').each(function() { $(this).text(JFX_MAJOR) });
+        $('.JDK_VERSION').each(function() { $(this).text(JDK_VERSION) });
+        $('.JFX_VERSION').each(function() { $(this).text(JFX_VERSION) });
+        $('.samples').each(function() { $(this).attr("href", SAMPLES + $(this).attr('href')); });
+        
+        var div_top = $('.content').offset().top;
+        $(window).scroll(function() {
+            var windowTop = $(window).scrollTop();
+            if (windowTop > div_top) {
+                $('.sidenav').css('top', windowTop);  
+            } else {
+                $('.sidenav').css('top', div_top);  
+            } 
         });
     });
 
@@ -83,9 +134,13 @@ $(function() {
 
                 // Remove active from all links in nav-bar
                 $('a[class*="list-group-item"]').removeClass("active");
+                // Remove active from all links in nav-bar
+                $('li[class*="list-group-item"]').removeClass("active");
                 
                 if ($(hyperlink).hasClass('list-group-item')) {
                     $(hyperlink).addClass("active");
+                } else if ($(hyperlink).parent(".checked").hasClass('list-group-item')) {
+                    $(hyperlink).parent(".checked").addClass("active");
                 }
             }
         }
