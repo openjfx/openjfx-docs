791b545734ab48c8abe7376fd1d5ba4fdb144fad
Abhinay Agarwal <abhinay_agarwal@live.com>
2020-01-17 16:28:55 +0530

    Update javafx-maven-plugin version to 0.0.4 (#132)

--- js/gluon.js
+++ js/gluon.js
@@ -6,7 +6,7 @@ $(function() {
     var JDK_VERSION = "13";
     var JFX_VERSION = "13";
     var JFX_PLUGIN_VERSION = "0.0.8";
-    var JFX_MVN_PLUGIN_VERSION = "0.0.3";
+    var JFX_MVN_PLUGIN_VERSION = "0.0.4";
     var JFX_MVN_ARCH_VERSION = "0.0.1";
     var JLINK_PLUGIN_VERSION = "2.12.0";
 
